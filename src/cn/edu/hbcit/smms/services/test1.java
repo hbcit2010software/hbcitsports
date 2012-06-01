48ea2b9b6a2842e3b99e283446d40915f8fe562a
liwei5946 <liwei5946@gmail.com>
2012-06-01 17:06:05 +0800

    del something

--- src/cn/edu/hbcit/smms/services/test1.java
+++
@@ -1,5 +0,0 @@
-package cn.edu.hbcit.smms.services;
-
-public class test1 {
-	String name = null;
-}
--- src/cn/edu/hbcit/smms/servlet/test1.java
+++
@@ -1,5 +0,0 @@
-package cn.edu.hbcit.smms.servlet;
-
-public class test1 {
-	String name = null;
-}
