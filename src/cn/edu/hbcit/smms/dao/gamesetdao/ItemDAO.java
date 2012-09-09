b516de660c4254419d2144849cf2ac6c768f5a32
liwei5946 <liwei5946@gmail.com>
2012-09-10 01:51:41 +0800

    拆分项目t_finalitem

--- src/cn/edu/hbcit/smms/dao/gamesetdao/ItemDAO.java
+++ src/cn/edu/hbcit/smms/dao/gamesetdao/ItemDAO.java
@@ -14,6 +14,7 @@ package cn.edu.hbcit.smms.dao.gamesetdao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
@@ -232,6 +233,74 @@ public class ItemDAO {
 			log.error(e.getMessage());
 		}
 		return list;
+	}
+	
+	public boolean splitFinalitem(int sportsId){
+		boolean flag = false;
+		//查询指定运动会ID的gp2spid和matchtype
+		String sql_QueryGp2spidAndMatchtype = "SELECT t_group2item.id,t_group.groupname ,t_item.itemname,t_group2item.matchtype FROM t_group2item,t_group2sports,t_item,t_group WHERE t_group2sports.id=t_group2item.gp2spid AND t_group2item.itemid=t_item.id AND t_group2sports.groupid=t_group.id AND t_group2sports.sportsid=?";
+		//插入拆分后的t_finalitem
+		String sql_InsertFinalitem = "INSERT INTO t_finalitem (gp2itid,finalitemname,finalitemtype,sportsid) VALUES (?,?,?,?)";
+		int count[]; 
+		try{
+			conn = db.getConn();
+			pStatement = conn.prepareStatement(sql_QueryGp2spidAndMatchtype);
+			pStatement.setInt(1, sportsId);
+			rs = pStatement.executeQuery();
+			
+			//以下为批量处理用
+			PreparedStatement tempPs = null;
+			Connection tempConn = null;
+			tempConn = db.getConn();
+			tempConn.setAutoCommit(false);// 需要用到事务，不能让他自动提交，需要手动提交
+			tempPs = tempConn.prepareStatement(sql_InsertFinalitem);
+			while(rs.next()){
+				if(rs.getInt(4) == 1){  //如果检索出来matchtype是“预决赛”
+					tempPs.setInt(1, rs.getInt(1));//gp2itid
+					tempPs.setString(2, rs.getString(2) + rs.getString(3) + "预决赛");//finalitemname
+					tempPs.setString(3, "3");//finalitemtype  3代表预决赛
+					tempPs.setInt(4, sportsId);//sportsid
+					tempPs.addBatch();
+				}
+				if(rs.getInt(4) == 2){  //如果检索出来matchtype是“预赛+决赛”,则添加两条记录
+					tempPs.setInt(1, rs.getInt(1));//gp2itid
+					tempPs.setString(2, rs.getString(2) + rs.getString(3) + "预赛");//finalitemname
+					tempPs.setString(3, "1");//finalitemtype  3代表预决赛
+					tempPs.setInt(4, sportsId);//sportsid
+					tempPs.addBatch();
+					//
+					tempPs.setInt(1, rs.getInt(1));//gp2itid
+					tempPs.setString(2, rs.getString(2) + rs.getString(3) + "决赛");//finalitemname
+					tempPs.setString(3, "2");//finalitemtype  3代表预决赛
+					tempPs.setInt(4, sportsId);//sportsid
+					tempPs.addBatch();
+				}
+			}
+			count = tempPs.executeBatch();
+			tempConn.commit();
+			
+			for(int i : count){  
+			    if(i == 0) {
+			    	tempConn.rollback();              // 回滚，非常重要 
+			    	log.error("分割生成Finalitem出现异常，回滚=========》");
+			     //System.out.println("======出现异常，回滚=========");  
+			    }  
+			   } 
+			tempPs.close();
+			tempConn.close();
+		}catch(SQLException e){
+			try{
+				// 回滚，非常重要  
+				conn.rollback();
+			}catch(SQLException e1){
+				log.error(e1.getMessage());
+			}
+			log.error("获取指定某届运动会的项目信息失败！splitFinalitem()");
+			log.error(e.getMessage());
+		}finally{
+			db.freeConnection(rs,pStatement,conn);
+		}
+		return flag;
 	}
 	
 }
